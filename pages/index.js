550e5d7429f2ee9898ccef293849113a773ef8ff
Martial Wehrling <wehrling@free.fr>
2022-05-27 14:29:39 +0300

    Modification totale des scripts  pour localisation la langue française sur les pays francophones : pays et capitale

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,35 @@ import Data from "../data/pictures";
 export default function Home(props) {
   console.log(props.location);
 
-  // veification de la langue du pays français ou anglais
-  const languageSearch = () => {
-    for (let i = 0; i < props.countrie.length; i++) {
-      if (props.countrie[i].en === props.location.country_name) {
-        return props.countrie[i].fr;
-      }
-      if (props.countrie[i].en !== props.location.country_name) {
-        props.location.country_name;
+  //verification si le sigle fr est présent dans languages de location
+  const languagesLocation = props.location.languages.includes("fr");
+
+  //verification pays de location ets identique a pays de country en anglais et on retourne en français
+  const countryFrench = () => {
+    for (let i = 0; i < props.country.length; i++) {
+      if (props.country[i].en === props.location.country_name) {
+        return props.country[i].fr;
       }
     }
   };
-  const countryFrench = languageSearch();
-  // affichage de l'attribu image en fonction du drapeau de localisation
+
+  //On assigne la fonction du pays français à une variable pour faciliter l'utilisation
+  const countryLanguageFrench = countryFrench();
+  console.log(countryLanguageFrench);
+
+  //verification on verifie que pays de city est identique au pays de la variable countryFrench pour retourner la capitale en français
+  const cityFrench = () => {
+    for (let i = 0; i < props.city.length; i++) {
+      if (countryLanguageFrench === props.city[i].pays_FR) {
+        return props.city[i].capitale;
+      }
+    }
+  };
+  //On assigne la fonction du ville français à une variable pour faciliter l'utilisation
+  const cityLanguageFrench = cityFrench();
+  console.log(cityLanguageFrench);
+
+  // affichage de l'attribut image en fonction du drapeau de localisation
   const flagAlt = `Drapeau de ${props.location.country_name}`;
 
   return (
@@ -42,24 +58,28 @@ export default function Home(props) {
             <h2 className="mx-2 mt-6 mb-2 text-left text-slate-100">
               M&Eacute;T&Eacute;O DE VOTRE G&Eacute;OLOCALISATION:
             </h2>
-            <div className="px-1 mr-1 ml-1 border-2 border-slate-100 rounded w-auto bg-gray-50/[0.9] drop-shadow-lg">
-              <ul className="flex space-x-12 items-center">
+            <div className="px-1 py- 8 mr-1 ml-1 border-2 border-slate-100 rounded w-auto bg-gray-50/[0.9] drop-shadow-lg">
+              <ul className="flex space-x-12 space-y-1 mr-4 items-center">
                 <li>
                   <img
-                    className="border-2 border-gray-500 rounded"
+                    className="border-2 border-gray-500 rounded min-80 min-64"
                     src={props.location.country_flag}
                     alt={flagAlt}
                   />
                 </li>
                 <li>
-                  <span className="mr-1"> Pays :</span>
-                  <span className="font-semibold">{countryFrench}</span>
+                  <span className="mr-1">Pays:</span>
+                  <span className="font-semibold">
+                    {languagesLocation
+                      ? countryLanguageFrench
+                      : props.location.country_name}
+                  </span>
                 </li>
                 <li>
-                  <span className="mr-1">Capitale du pays :</span>
+                  <span className="mr-1">Capitale:</span>
                   <span className="font-semibold">
-                    {props.location.city === ""
-                      ? props.location.country_capital
+                    {languagesLocation
+                      ? cityLanguageFrench
                       : props.location.city}
                   </span>
                 </li>
@@ -84,16 +104,20 @@ export async function getStaticProps() {
   const key = "7bee4c110b8a43849ebeb6b837154eae";
   const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${key}`;
 
-  const response = await fetch(url);
-  const location = await response.json();
+  const data1 = await fetch(url);
+  const location = await data1.json();
 
-  const data = await import("../data/countries.json");
-  const countrie = data.countries;
+  const data2 = await import("../data/countries.json");
+  const country = data2.countries;
+
+  const data3 = await import("../data/cities.json");
+  const city = data3.cities;
 
   return {
     props: {
       location,
-      countrie,
+      country,
+      city,
     },
   };
 }
