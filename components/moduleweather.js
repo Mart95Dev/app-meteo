3daada3895c5865d13472259ad407e08a124c731
Martial Wehrling <wehrling@free.fr>
2022-06-06 15:43:56 +0300

    ajout du cossier et fichiers components et renomage des noms de function comme les fichiers en lowercase

---
+++ components/moduleweather.js
@@ -0,0 +1,89 @@
+import Image from "next/image";
+
+export default function moduleweather({ weather, translator, browser }) {
+  const urlIcon = `http:${weather.current.condition.icon}`;
+  const fileIcon = urlIcon.slice(44);
+  // const test1 = "";
+
+  const myLoader2 = ({ src, width, quality }) => {
+    return `http://cdn.weatherapi.com/weather/64x64/day/${src}?w=${width}&q=${
+      quality || 75
+    }`;
+  };
+
+  return (
+    <div className="mt-1 text-sm ">
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-center bg-slate-50 drop-shadow-lg rounded">
+          <span className="leading-loose">
+            {weather.current.condition.text}
+          </span>
+        </div>
+        <div className="flex space-x-16">
+          <div className="flex flex-row place-items-center">
+            <span>
+              <Image
+                loader={myLoader2}
+                src={fileIcon}
+                width={64}
+                height={64}
+                alt={"icon qui siginifie " + weather.current.condition.text}
+              />
+            </span>
+          </div>
+          <div className="flex flex-col">
+            <span className=" text-center leading-loose text-slate-500">
+              {browser === "fr" ? translator[9].temp_fr : translator[9].temp_en}
+            </span>
+            <span className="text-center leading-loose text-lg text-sky-400">
+              {weather.current.temp_c + "°"}
+            </span>
+            <span className="text-center leading-loose text-slate-500">
+              {browser === "fr"
+                ? translator[10].precip_mm_fr
+                : translator[10].precip_mm_en}
+            </span>
+            <span className=" text-centerleading-loose text-lg text-sky-400">
+              {weather.current.precip_mm + "mm"}
+            </span>
+          </div>
+          <div className="flex flex-col">
+            <span className=" text-center leading-loose text-slate-500">
+              {browser === "fr"
+                ? translator[8].feelslike_fr
+                : translator[8].feelslike_en}
+            </span>
+            <span className="text-center leading-loose text-lg text-sky-400">
+              {weather.current.feelslike_c + "°"}
+            </span>
+            <span className="text-center leading-loose text-slate-500">
+              {browser === "fr"
+                ? translator[11].wind_speed_fr
+                : translator[11].wind_speed_en}
+            </span>
+            <span className="text-center leading-loose text-lg text-sky-400">
+              {weather.current.wind_kph + " kph "}
+              {weather.current.wind_dir}
+            </span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-center leading-loose text-slate-500">
+              {browser === "fr"
+                ? translator[12].humidity_fr
+                : translator[12].humidity_en}
+            </span>
+            <span className="text-center leading-loose text-lg text-sky-400">
+              {weather.current.humidity + " %"}
+            </span>
+            <span className="text-center leading-loose text-slate-500">
+              {browser === "fr" ? translator[13].uv_fr : translator[13].uv_en}
+            </span>
+            <span className="text-center leading-loose text-lg text-sky-400">
+              {weather.current.uv}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/usefetch.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import axios from "axios";
+
+// Hook personnalisé pour appeler l'api weather en fonction de la recherche
+
+function usefetch(url) {
+  const [dbWeather, setDbWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // const test = "";
+
+  const searchWeather = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setDbWeather(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { dbWeather, loading, error, searchWeather };
+}
+
+export default UseFetch;
