9d3df4bcc9768b24b11d58c877144342bbc08b50
Martial Wehrling <wehrling@free.fr>
2022-06-06 09:52:15 +0300

    ajour library next-image-export-optimizer et modification de nextConfig

--- next.config.js
+++ next.config.js
@@ -10,10 +10,23 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
   nextConfig,
+  images: {
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    nextImageExportOptimizer: {
+      imageFolderPath: "public/images",
+      exportFolderPath: "out",
+      quality: 75,
+      domains: ["cdn.weatherapi.com", "ipgeolocation.io"],
+    },
+  },
+  env: {
+    storePicturesInWEBP: true,
+    generateAndUseBlurImages: true,
+  },
+
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-  images: {
-    domains: ["cdn.weatherapi.com", "ipgeolocation.io"],
   },
 };
